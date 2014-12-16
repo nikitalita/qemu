c5e83004bf17eed66715367f5ef4cbd3df580b3e
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2014-12-16 22:04:19 +0000

    macio: very very close! numbers look right-ish now.

--- hw/ide/macio.c
+++ hw/ide/macio.c
@@ -31,7 +31,7 @@
 #include <hw/ide/internal.h>
 
 /* debug MACIO */
-#define DEBUG_MACIO
+//#define DEBUG_MACIO
 
 #ifdef DEBUG_MACIO
 static const int debug_macio = 1;
@@ -91,6 +91,11 @@ static void pmac_dma_read(BlockBackend *blk,
 	s->io_buffer_index += transfer;
 	s->io_buffer_size -= transfer;
 	
+	if (io->remainder_len != 0) {
+            /* Still waiting for remainder */
+	    return;
+	}	
+	
 	if (io->len == 0) {
 	    MACIO_DPRINTF("--- finished all read processing; go and finish\n");
 	    cb(opaque, 0);
@@ -133,7 +138,7 @@ static void pmac_dma_read(BlockBackend *blk,
     //io->addr += io->len;
     io->len = 0;
 
-    MACIO_DPRINTF("--- Block transfer   - sector_num: %lx  nsector: %x\n", sector_num, nsector);
+    MACIO_DPRINTF("--- Block read transfer   - sector_num: %lx  nsector: %x\n", sector_num, nsector);
     
     m->aiocb = blk_aio_readv(blk, sector_num, &io->iov, nsector,
                               cb, io);
@@ -158,13 +163,13 @@ static void pmac_dma_write(BlockBackend *blk,
         /* Return remainder of request */
 	int transfer = MIN(io->remainder_len, io->len);
 	
-	//printf("--- processing write remainder %x\n", transfer);
+	MACIO_DPRINTF("--- processing write remainder %x\n", transfer);
 	cpu_physical_memory_read(io->addr, &io->remainder + (0x200 - transfer), transfer);
 		
 	io->remainder_len -= transfer;
 	io->len -= transfer;
 	io->addr += transfer;
-	
+
 	s->io_buffer_index += transfer;
 	s->io_buffer_size -= transfer;
 	
@@ -173,11 +178,11 @@ static void pmac_dma_write(BlockBackend *blk,
 	    return;
 	}
 	
-	printf("--> prepending bounce buffer with size 0x200\n");
+	MACIO_DPRINTF("--> prepending bounce buffer with size 0x200\n");
 		
 	/* Sector transfer complete - prepend to request */
 	qemu_iovec_add(&io->iov, &io->remainder, 0x200);
-        extra = 1;
+	extra = 1;
     }    
     
     if (s->drive_kind == IDE_CD) {
@@ -186,11 +191,13 @@ static void pmac_dma_write(BlockBackend *blk,
 	    sector_num = ide_get_sector(s) + (s->io_buffer_index >> 9);
     }
     
-    nsector = ((io->len + 0x1ff) >> 9);
+    nsector = (io->len >> 9);
     
-    remainder = (nsector << 9) - io->len;
+    remainder = io->len - (nsector << 9);
     
     MACIO_DPRINTF("--- DMA write transfer - addr: %" HWADDR_PRIx " len: %x\n", io->addr, io->len);
+    MACIO_DPRINTF("xxx remainder: %x\n", remainder);
+    MACIO_DPRINTF("xxx sector_num: %lx   nsector: %x\n", sector_num, nsector);
     
     dma_addr = io->addr;
     dma_len = io->len;
@@ -202,27 +209,29 @@ static void pmac_dma_write(BlockBackend *blk,
     } else {
         //printf("--- %s write remainder: %x\n", __func__, remainder);
 	
-	/* Write up to last complete sector */
-	nsector--;
-	
+	/* Write up to last complete sector */	
         MACIO_DPRINTF("--- DMA write unaligned - addr: %" HWADDR_PRIx " len: %x\n", io->addr, (nsector << 9));
 	qemu_iovec_add(&io->iov, mem, (nsector << 9));
 	
-	MACIO_DPRINTF("--- DMA write read    - bounce addr: %p remainder_len: %x\n", &io->remainder, (0x200 - remainder));
-	cpu_physical_memory_read(io->addr + (nsector << 9), &io->remainder, (0x200 - remainder));
+	MACIO_DPRINTF("--- DMA write read    - bounce addr: %p remainder_len: %x\n", &io->remainder, remainder);
+	cpu_physical_memory_read(io->addr + (nsector << 9), &io->remainder, remainder);
 	
 	//printf(".. qiov size is %lx, nsector is %x\n", io->iov.size, nsector);
 	
-        io->remainder_len = remainder;
+        io->remainder_len = 0x200 - remainder;
+	
+	MACIO_DPRINTF("xxx remainder_len: %x\n", io->remainder_len);
     }
 
-    s->io_buffer_size -= io->len;
-    s->io_buffer_index += io->len;
+    s->io_buffer_size -= ((nsector + extra) << 9);
+    s->io_buffer_index += ((nsector + extra) << 9);
 
     //io->addr += io->len;
     io->len = 0;
+
+    MACIO_DPRINTF("--- Block write transfer   - sector_num: %lx  nsector: %x\n", sector_num, nsector + extra);    
     
-    m->aiocb = blk_aio_writev(blk, sector_num - extra, &io->iov, nsector + extra,
+    m->aiocb = blk_aio_writev(blk, sector_num, &io->iov, nsector + extra,
                               cb, io);
 }
 
