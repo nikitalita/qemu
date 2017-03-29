73b69a30896bc053999450bcfc12a13fb5a4cd2d
Gerd Hoffmann <kraxel@redhat.com>
2017-03-29 12:10:54 +0200

    memory: add support getting and using a dirty bitmap copy.
    This patch adds support for getting and using a local copy of the dirty
    bitmap.
    
    memory_region_copy_and_clear_dirty() will create a copy of the dirty
    bitmap for the specified range, clear the dirty bitmap and return the
    copy.  The returned bitmap can be a bit larger than requested, the range
    is expanded so the code can copy unsigned longs from the bitmap and
    avoid atomic bit update operations.
    
    memory_region_copy_get_dirty() will return the dirty status of pages,
    pretty much like memory_region_get_dirty(), but using the copy returned
    by memory_region_copy_and_clear_dirty().
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -871,6 +871,22 @@ void memory_region_set_dirty(MemoryRegion *mr, hwaddr addr,
  */
 bool memory_region_test_and_clear_dirty(MemoryRegion *mr, hwaddr addr,
                                         hwaddr size, unsigned client);
+
+/**
+ * memory_region_snapshot_and_clear_dirty: TODO
+ */
+DirtyBitmapSnapshot *memory_region_snapshot_and_clear_dirty(MemoryRegion *mr,
+                                                            hwaddr addr,
+                                                            hwaddr size,
+                                                            unsigned client);
+
+/**
+ * memory_region_snapshot_get_dirty: TODO
+ */
+bool memory_region_snapshot_get_dirty(MemoryRegion *mr,
+                                      DirtyBitmapSnapshot *snap,
+                                      hwaddr addr, hwaddr size);
+
 /**
  * memory_region_sync_dirty_bitmap: Synchronize a region's dirty bitmap with
  *                                  any external TLBs (e.g. kvm)
