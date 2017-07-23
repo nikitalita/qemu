a3d0ef5aa4bfbb617f0b1eedbbdbd3314462da7b
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2017-07-23 17:47:56 +0100

    hme: implement MII registers

--- hw/net/hme.c
+++ hw/net/hme.c
@@ -25,25 +25,47 @@
 #include "qemu/osdep.h"
 #include "hw/hw.h"
 #include "hw/pci/pci.h"
+#include "hw/net/mii.h"
 #include "net/net.h"
 
-#define HME_REG_SIZE       0x8000
+#define HME_REG_SIZE           0x8000
 
-#define HME_SEB_REG_SIZE   0x2000
+#define HME_SEB_REG_SIZE       0x2000
 
-#define HME_SEBI_RESET     0x0
-#define HME_SEB_RESET_ETX  0x1
-#define HME_SEB_RESET_ERX  0x2
+#define HME_SEBI_RESET         0x0
+#define HME_SEB_RESET_ETX      0x1
+#define HME_SEB_RESET_ERX      0x2
 
-#define HME_ETX_REG_SIZE   0x2000
-#define HME_ERX_REG_SIZE   0x2000
-#define HME_MAC_REG_SIZE   0x1000
+#define HME_ETX_REG_SIZE       0x2000
+#define HME_ERX_REG_SIZE       0x2000
+#define HME_MAC_REG_SIZE       0x1000
 
-#define HME_MIF_REG_SIZE   0x20
+#define HME_MIF_REG_SIZE       0x20
 
-#define HME_MIFI_CFG       0x10
-#define HME_MIF_CFG_MDI0   0x100
-#define HME_MIF_CFG_MDI1   0x200 
+#define HME_MIFI_FO            0xc
+#define HME_MIF_FO_ST          0xc0000000    /* Start of frame */
+#define HME_MIF_FO_ST_SHIFT    30
+#define HME_MIF_FO_OPC         0x30000000    /* Opcode */
+#define HME_MIF_FO_OPC_SHIFT   28
+#define HME_MIF_FO_PHYAD       0x0f800000    /* PHY Address */
+#define HME_MIF_FO_PHYAD_SHIFT 23
+#define HME_MIF_FO_REGAD       0x007c0000    /* Register Address */
+#define HME_MIF_FO_REGAD_SHIFT 18
+#define HME_MIF_FO_TAMSB       0x20000    /* Turn-around MSB */
+#define HME_MIF_FO_TALSB       0x10000    /* Turn-around LSB */
+#define HME_MIF_FO_DATA        0xffff     /* data to read or write */
+
+#define HME_MIFI_CFG           0x10
+#define HME_MIF_CFG_MDI0       0x100
+#define HME_MIF_CFG_MDI1       0x200 
+
+/* Wired HME PHY addresses */
+#define	HME_PHYAD_INTERNAL     1
+#define	HME_PHYAD_EXTERNAL     0
+
+#define MII_COMMAND_START      0x1
+#define MII_COMMAND_READ       0x2
+#define MII_COMMAND_WRITE      0x1
 
 
 /* Change to 1 to enable debugging */
@@ -77,6 +99,8 @@ typedef struct HMEState {
     uint32_t erxregs[HME_ERX_REG_SIZE >> 2];
     uint32_t macregs[HME_MAC_REG_SIZE >> 2];
     uint32_t mifregs[HME_MIF_REG_SIZE >> 2];
+    
+    uint16_t miiregs[0x20];
 } HMEState;
 
 static Property hme_properties[] = {
@@ -228,11 +252,36 @@ static const MemoryRegionOps hme_mac_ops = {
     },
 };
 
+static void hme_mii_write(HMEState *s, uint8_t reg, uint16_t data)
+{
+    DPRINTF("hme_mii_write %x %x\n", reg, data);
+    
+    switch (reg) {
+    case MII_BMCR:
+        /* Autoclear reset bit */
+        if (data & MII_BMCR_RESET) {
+            data &= ~MII_BMCR_RESET;
+        }
+        break;
+    }
+    
+    s->miiregs[reg] = data;
+}
+
+static uint16_t hme_mii_read(HMEState *s, uint8_t reg)
+{
+    DPRINTF("hme_mii_read %x %x\n", reg, s->miiregs[reg]);
+
+    return s->miiregs[reg];
+}
+
 static void hme_mif_write(void *opaque, hwaddr addr,
                           uint64_t val, unsigned size)
 {
     HMEState *s = HME(opaque);
-
+    uint8_t cmd, reg;
+    uint16_t data;
+    
     DPRINTF("hme_mif_write %" HWADDR_PRIx " %lx\n", addr, val);
 
     switch (addr) {
@@ -241,6 +290,37 @@ static void hme_mif_write(void *opaque, hwaddr addr,
         val &= ~(HME_MIF_CFG_MDI0 | HME_MIF_CFG_MDI1);
         val |= s->mifregs[HME_MIFI_CFG >> 2] &
                (HME_MIF_CFG_MDI0 | HME_MIF_CFG_MDI1);
+        break;
+
+    case HME_MIFI_FO:
+        /* Detect start of MII command */
+        if ((val & HME_MIF_FO_ST) >> HME_MIF_FO_ST_SHIFT
+            != MII_COMMAND_START) {
+            break;
+        }
+        
+        /* Internal phy only */
+        if ((val & HME_MIF_FO_PHYAD) >> HME_MIF_FO_PHYAD_SHIFT
+            != HME_PHYAD_INTERNAL) {
+            break;
+        }
+        
+        cmd = (val & HME_MIF_FO_OPC) >> HME_MIF_FO_OPC_SHIFT;
+        reg = (val & HME_MIF_FO_REGAD) >> HME_MIF_FO_REGAD_SHIFT;
+        data = (val & HME_MIF_FO_DATA);
+
+        switch (cmd) {
+        case MII_COMMAND_WRITE:
+            hme_mii_write(s, reg, data);
+            break;
+
+        case MII_COMMAND_READ:
+            val &= ~HME_MIF_FO_DATA;
+            val |= hme_mii_read(s, reg);
+            break;
+        }
+
+        val |= HME_MIF_FO_TALSB;        
         break;
     }
 
@@ -324,7 +404,11 @@ static void hme_reset(DeviceState *ds)
     printf("hme reset!\n");
 
     /* Configure internal transceiver */
-    s->mifregs[HME_MIFI_CFG >> 2] |= HME_MIF_CFG_MDI0;   
+    s->mifregs[HME_MIFI_CFG >> 2] |= HME_MIF_CFG_MDI0;
+    
+    /* Allow 100Mbps FD */
+    s->miiregs[MII_BMSR] = MII_BMSR_AN_COMP | MII_BMSR_LINK_ST |
+                           MII_BMSR_100TX_FD;
 }
 
 static void hme_class_init(ObjectClass *klass, void *data)
