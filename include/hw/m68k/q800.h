225e3161c716559d4dfec88a5fb2ad7a882829fa
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2019-12-15 10:11:09 +0000

    q800: move ROM memory region to Q800MachineState
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- include/hw/m68k/q800.h
+++ include/hw/m68k/q800.h
@@ -31,6 +31,7 @@ typedef struct Q800MachineState {
     MachineState parent_obj;
 
     M68kCPU *cpu;
+    MemoryRegion rom;
 } Q800MachineState;
 
 #define TYPE_Q800_MACHINE MACHINE_TYPE_NAME("q800")
