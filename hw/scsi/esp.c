bad61c065c6ee886d6619fddbb3fbe921b299b3f
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2020-12-07 16:03:15 +0000

    esp: set TC when dmalen == 0

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -190,6 +190,10 @@ static void esp_set_tc(ESPState *s, uint32_t dmalen)
     s->rregs[ESP_TCLO] = dmalen & 0xff;
     s->rregs[ESP_TCMID] = dmalen >> 8;
     s->rregs[ESP_TCHI] = dmalen >> 16;
+
+    if (dmalen == 0) {
+        s->rregs[ESP_RSTAT] |= STAT_TC;
+    }
 }
 
 static int esp_select(ESPState *s)
