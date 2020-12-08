c4d79ca10a50e97cee477275930d70753653cae3
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2020-12-08 21:19:06 +0000

    esp: reset FIFO at start of handle_ti

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -680,6 +680,8 @@ static void handle_ti(ESPState *s)
 
     trace_esp_handle_ti(dmalen);
     if (s->dma) {
+        s->ti_rptr = 0;
+        s->ti_wptr = 0;
         s->rregs[ESP_RSTAT] &= ~STAT_TC;
         esp_do_dma(s);
     } else if (s->do_cmd) {
