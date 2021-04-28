7e23a0bf653ecc87f0c3a6de156f492718a0c557
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2021-04-28 10:12:09 +0100

    WIP: add NetBSD symbols to aid -d in_asm output for debugging

--- hw/m68k/q800.c
+++ hw/m68k/q800.c
@@ -670,6 +670,15 @@ static void q800_init(MachineState *machine)
         }
         g_free(filename);
 
+        /* WIP: load netbsd-INSTALL file for symbols with executing with -d in_asm */
+        filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, "netbsd-GENERIC");
+        if (filename) {
+            load_elf(filename, NULL, NULL, NULL, &elf_entry, NULL, NULL, NULL,
+                     1, EM_68K, 0, 0);
+            fprintf(stderr, "Loaded netbsd-GENERIC symbols!\n");
+        }
+        g_free(filename);
+
         filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
         memory_region_add_subregion(get_system_memory(), MACROM_ADDR, rom);
 
